fc0bf357bf0d91797a80fc5cc878050398f5cef4
auzunder <edgarnogueiratavares@gmail.com>
2022-11-07 03:29:48 +0000

    update

--- servidor.js
+++ servidor.js
@@ -33,6 +33,7 @@ const logging = (req, res, next) => {
 // Tentar abrir ficheiro
     try {
         var navbar = fs.readFileSync('html/NavBar.html', 'utf-8');
+        var registo_login_popUp = fs.readFileSync('html/registo_login.html', 'utf-8');
         var footer = fs.readFileSync('html/Footer.html', 'utf-8');
     }
     // Caso nao consiga da log do erro
@@ -63,9 +64,9 @@ servidor.get("/", logging, function (req, res) {
     //Title
     html += '<title>XTRA FOOD</title>\n';
     html += '<link rel="stylesheet" href="css/fonts.css" type="text/css">\n';
-    html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/navbar_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/home_css.css" type="text/css">\n';
     //html += '\n<script src="home.js"></script>';
     //HTML close head
@@ -74,6 +75,8 @@ servidor.get("/", logging, function (req, res) {
     //HTML NavBar
     html += navbar;
     //html += '<div id="navbar_ghost_scpace"></div>'
+
+    html += registo_login_popUp;
 
     //HTML Content
     html += home_content;
