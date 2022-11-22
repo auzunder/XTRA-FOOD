26abdb4938078208974ad50a267a6a83dd4e2a53
auzunder <edgarnogueiratavares@gmail.com>
2022-11-22 14:37:27 +0000

    Home concluida

--- servidor.js
+++ servidor.js
@@ -87,3 +87,49 @@ servidor.get("/", logging, function (req, res) {
     html += '\n</body>\n</html>';
     res.send(html);
 });
+
+servidor.get("/voluntariado", logging, function (req, res) {
+    // Tentar abrir ficheiro
+    try {
+        var content = fs.readFileSync('html/voluntariado.html', 'utf-8');
+    }
+    // Caso nao consiga da log do erro
+    catch (error){
+        console.error("Erro ao ler ficheiros de conteudo.")
+        console.error(error)
+    }
+
+    var html = "";
+
+    //HTML head
+    html += '<!DOCTYPE html>\n<html lang=pt>\n<head>\n';
+    //HTML head meta
+    html += '<meta charset="utf-8">\n';
+    html += '<meta http-equiv="X-UA-Compatible" content="IE=edge">\n';
+    html += '<meta name="viewport" content="width=device-width, initial-scale=1">\n';
+    //Title
+    html += '<title>XTRA FOOD</title>\n';
+    html += '<link rel="stylesheet" href="css/fonts.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/navbar_css.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/voluntariado_css.css" type="text/css">\n';
+    html += '<script src="js/login.js"></script>\n';
+    //HTML close head
+    html += '</head>\n<body>';
+
+    //HTML NavBar
+    html += navbar;
+    //html += '<div id="navbar_ghost_scpace"></div>'
+
+    html += registo_login_popUp;
+
+    //HTML Content
+    html += content;
+
+    html += footer;
+
+    //HTML close
+    html += '\n</body>\n</html>';
+    res.send(html);
+});
