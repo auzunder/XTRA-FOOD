30f2368ad5f387f7848fa10510430b29e10efd1e
auzunder <edgarnogueiratavares@gmail.com>
2022-11-23 10:40:48 +0000

    PARCEIROS

--- servidor.js
+++ servidor.js
@@ -208,12 +208,59 @@ servidor.get("/sobre_nos", logging, function (req, res) {
     html += '<meta http-equiv="X-UA-Compatible" content="IE=edge">\n';
     html += '<meta name="viewport" content="width=device-width, initial-scale=1">\n';
     //Title
-    html += '<title>Doação | XTRA FOOD</title>\n';
+    html += '<title>Sobre Nós | XTRA FOOD</title>\n';
     html += '<link rel="stylesheet" href="css/fonts.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/navbar_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/sobre_nos_css.css" type="text/css">\n';
+    html += '<script src="js/login.js"></script>\n';
+    //HTML close head
+    html += '</head>\n<body>';
+
+    //HTML NavBar
+    html += navbar;
+    //html += '<div id="navbar_ghost_scpace"></div>'
+
+    html += registo_login_popUp;
+
+    //HTML Content
+    html += content;
+
+    html += footer;
+
+    //HTML close
+    html += '\n</body>\n</html>';
+    res.send(html);
+});
+
+// PAGINA SERVIÇOS
+servidor.get("/parceiros", logging, function (req, res) {
+    // Tentar abrir ficheiro
+    try {
+        var content = fs.readFileSync('html/servicos.html', 'utf-8');
+    }
+    // Caso nao consiga da log do erro
+    catch (error){
+        console.error("Erro ao ler ficheiros de conteudo.")
+        console.error(error)
+    }
+
+    var html = "";
+
+    //HTML head
+    html += '<!DOCTYPE html>\n<html lang=pt>\n<head>\n';
+    //HTML head meta
+    html += '<meta charset="utf-8">\n';
+    html += '<meta http-equiv="X-UA-Compatible" content="IE=edge">\n';
+    html += '<meta name="viewport" content="width=device-width, initial-scale=1">\n';
+    //Title
+    html += '<title>Serviços | XTRA FOOD</title>\n';
+    html += '<link rel="stylesheet" href="css/fonts.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/navbar_css.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/servicos_css.css" type="text/css">\n';
     html += '<script src="js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -256,7 +303,7 @@ servidor.get("/eventos", logging, function (req, res) {
     html += '<meta http-equiv="X-UA-Compatible" content="IE=edge">\n';
     html += '<meta name="viewport" content="width=device-width, initial-scale=1">\n';
     //Title
-    html += '<title>Doação | XTRA FOOD</title>\n';
+    html += '<title>Eventos | XTRA FOOD</title>\n';
     html += '<link rel="stylesheet" href="css/fonts.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/navbar_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
