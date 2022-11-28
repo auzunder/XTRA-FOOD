15a8ac5d12d16118c07b0a0b0edc614987a58ada
auzunder <edgarnogueiratavares@gmail.com>
2022-11-28 19:41:30 +0000

    começo loja

--- servidor.js
+++ servidor.js
@@ -328,3 +328,50 @@ servidor.get("/eventos", logging, function (req, res) {
     html += '\n</body>\n</html>';
     res.send(html);
 });
+
+// PAGINA LOJA
+servidor.get("/loja", logging, function (req, res) {
+    // Tentar abrir ficheiro
+    try {
+        var content = fs.readFileSync('html/loja.html', 'utf-8');
+    }
+    // Caso nao consiga da log do erro
+    catch (error){
+        console.error("Erro ao ler ficheiros de conteudo.")
+        console.error(error)
+    }
+
+    var html = "";
+
+    //HTML head
+    html += '<!DOCTYPE html>\n<html lang=pt>\n<head>\n';
+    //HTML head meta
+    html += '<meta charset="utf-8">\n';
+    html += '<meta http-equiv="X-UA-Compatible" content="IE=edge">\n';
+    html += '<meta name="viewport" content="width=device-width, initial-scale=1">\n';
+    //Title
+    html += '<title>Eventos | XTRA FOOD</title>\n';
+    html += '<link rel="stylesheet" href="css/fonts.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/navbar_css.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
+    html += '<link rel="stylesheet" href="css/loja_css.css" type="text/css">\n';
+    html += '<script src="js/login.js"></script>\n';
+    //HTML close head
+    html += '</head>\n<body>';
+
+    //HTML NavBar
+    html += navbar;
+    //html += '<div id="navbar_ghost_scpace"></div>'
+
+    html += registo_login_popUp;
+
+    //HTML Content
+    html += content;
+
+    html += footer;
+
+    //HTML close
+    html += '\n</body>\n</html>';
+    res.send(html);
+});
