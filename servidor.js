e601de85e1abe9ad1bb517c7b9f857a2d42ecc32
auzunder <edgarnogueiratavares@gmail.com>
2022-12-29 00:15:04 +0000

    cenas

--- servidor.js
+++ servidor.js
@@ -1,17 +1,16 @@
 const session = require('express-session');
 const express = require('express');
-const sha1 = require('sha1');
 const fs = require('fs');
+const mysql = require("mysql2");
+
+const fileUpload = require("express-fileupload");
 
 const servidor = express();
 var porta = 8080;
 
-servidor.use(express.urlencoded({
-    extended: true
-}));
-
+servidor.use(express.urlencoded({extended: true}));
 servidor.use(express.static("public"));
-
+servidor.use(fileUpload());
 //  Sessão
 servidor.use(session({
     secret: "supercalifragilisticexpialidocious",
@@ -20,21 +19,37 @@ servidor.use(session({
 }));
 
 // Ligação ao servidor
+var porta = 8080;
 servidor.listen(porta, function () {
     console.log("servidor a ser executado em http://localhost:" + porta);
 });
 
+// MySQL user - database
+var pool = mysql.createPool({
+    host: "localhost",
+    user: "xtra_food",
+    password: "xtra_food",
+    database: "xtra_food",
+    charset: "utf8",
+    multipleStatements: true
+});
+
+
+//LOG SYSTEM
 const logging = (req, res, next) => {
-    console.log("Path: ", req.session);
+    console.log('================================================================================================================================================');
     console.log("Date: ", Date());
-    next()
-}
+    console.log("Path: ", req.session);
+    console.log('================================================================================================================================================');
+    next();
+};
 
 // Tentar abrir fichieros prinicipais
     try {
         var navbar = fs.readFileSync('html/NavBar.html', 'utf-8');
         var registo_login_popUp = fs.readFileSync('html/registo_login.html', 'utf-8');
         var footer = fs.readFileSync('html/Footer.html', 'utf-8');
+       // var err_404 = fs.readFileSync('','utf-8');
     }
     // Caso nao consiga da log do erro
     catch (error){
@@ -44,6 +59,7 @@ const logging = (req, res, next) => {
 
 // PAGINA INICIAL
 servidor.get("/", logging, function (req, res) {
+    var query ='';
     // Tentar abrir ficheiro
     try {
         var home_content = fs.readFileSync('html/home.html', 'utf-8');
@@ -53,9 +69,9 @@ servidor.get("/", logging, function (req, res) {
         console.error("Erro ao ler ficheiros de conteudo.")
         console.error(error)
     }
-
+    
     var html = "";
-
+    
     //HTML head
     html += '<!DOCTYPE html>\n<html lang=pt>\n<head>\n';
     //HTML head meta
@@ -64,6 +80,8 @@ servidor.get("/", logging, function (req, res) {
     html += '<meta name="viewport" content="width=device-width, initial-scale=1">\n';
     //Title
     html += '<title>XTRA FOOD</title>\n';
+    html += '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">\n';
+    html += '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js">\n</script>\n';
     html += '<link rel="stylesheet" href="css/fonts.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/navbar_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
@@ -73,21 +91,48 @@ servidor.get("/", logging, function (req, res) {
     html += '<script src="js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
-
+    
+    
     //HTML NavBar
     html += navbar;
-    //html += '<div id="navbar_ghost_scpace"></div>'
-
-    html += registo_login_popUp;
-
+    if (req.session.username){
+        html += '<a href="/user" id="userAccount_navbar" class="flex flex_row flex_center navBarMenuOption"> <img class="navBarIcons" src="images/navbar icons/contaIcon.png" alt="icone conta"></a>';
+         html += '</div></div></header>';
+    }else{
+        html += '<div id="userAccount_navbar" class="flex flex_row flex_center navBarMenuOption pointer" onclick="session_abrir('+"'"+'registo_popUp'+"'"+')"> <img class="navBarIcons" src="images/navbar icons/contaIcon.png" alt="icone conta"></div>'; 
+        html += '</div></div></header>';
+        html += registo_login_popUp;
+    }
+    
     //HTML Content
     html += home_content;
+    
+    query += 'SELECT cartaz_img FROM evento ORDER BY data asc;';
+    pool.query(query, function (err, result, fields) {
+        if (!err) {
+            if (result && result.length > 0) {
+                console.log(result)
+                html += '\n<script>';
+                
+                for (var index in result){
+                    html += 'document.getElementById("cartaz_' + (parseInt(index)+1).toString() + '").src = "images/eventos/' + (parseInt(index)+1).toString() + '.jpg";\n';
+                }
+                //html += 'document.getElementById("cartaz_2")';
+                //html += 'document.getElementById("cartaz_3")';
 
-    html += footer;
-
-    //HTML close
-    html += '\n</body>\n</html>';
-    res.send(html);
+                html += '\n</script>\n';
+            }else {
+                console.log('Não foi possível obter resultados')
+            };
+        }
+        else {
+            console.log(err)
+            console.log('Erro ao executar pedido ao servidor')
+        }
+        html += footer;
+        html += '</body>\n</html>';
+        res.send(html);
+    });
 });
 
 
@@ -118,6 +163,7 @@ servidor.get("/voluntariado", logging, function (req, res) {
     html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/voluntariado_css.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/images/logos/main.png">';
     html += '<script src="js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -168,6 +214,7 @@ servidor.get("/doacao", logging, function (req, res) {
     html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/doacao_css.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/images/logos/main.png">';
     html += '<script src="js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -215,6 +262,7 @@ servidor.get("/sobre_nos", logging, function (req, res) {
     html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/sobre_nos_css.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/images/logos/main.png">';
     html += '<script src="js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -262,6 +310,7 @@ servidor.get("/parceiros", logging, function (req, res) {
     html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/servicos_css.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/images/logos/main.png">';
     html += '<script src="js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -310,6 +359,7 @@ servidor.get("/eventos", logging, function (req, res) {
     html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/eventos_css.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/images/logos/main.png">';
     html += '<script src="js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -357,6 +407,7 @@ servidor.get("/loja", logging, function (req, res) {
     html += '<link rel="stylesheet" href="css/footer_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/general_styles.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="css/loja_css.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/images/logos/main.png">';
     html += '<script src="js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -408,6 +459,7 @@ servidor.get("/loja/produto/ultrafood", logging, function (req, res) {
     html += '<link rel="stylesheet" href="/../css/general_styles.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="/../css/loja_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="/../css/produto_css.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/../images/logos/main.png">';
     html += '<script src="/../js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -460,6 +512,7 @@ servidor.get("/loja/carrinho", logging, function (req, res) {
     html += '<link rel="stylesheet" href="/../css/carrinho_css.css" type="text/css">\n';
     //html += '<link rel="stylesheet" href="/../css/loja_css.css" type="text/css">\n';
     //html += '<link rel="stylesheet" href="/../css/produto_css.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/../images/logos/main.png">';
     html += '<script src="/../js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -512,6 +565,7 @@ servidor.get("/loja/carrinho/informacao", logging, function (req, res) {
     html += '<link rel="stylesheet" href="/../css/carrinho_css.css" type="text/css">\n';
     //html += '<link rel="stylesheet" href="/../css/loja_css.css" type="text/css">\n';
     //html += '<link rel="stylesheet" href="/../css/produto_css.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/../images/logos/main.png">';
     html += '<script src="/../js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -564,6 +618,7 @@ servidor.get("/loja/carrinho/finalizacao", logging, function (req, res) {
     html += '<link rel="stylesheet" href="/../css/carrinho_css.css" type="text/css">\n';
     //html += '<link rel="stylesheet" href="/../css/loja_css.css" type="text/css">\n';
     //html += '<link rel="stylesheet" href="/../css/produto_css.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/../images/logos/main.png">';
     html += '<script src="/../js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -613,6 +668,7 @@ servidor.get("/user", logging, function (req, res) {
     html += '<link rel="stylesheet" href="/../css/navbar_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="/../css/footer_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="/../css/general_styles.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/../images/logos/main.png">';
     html += '<script src="/../js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
@@ -664,6 +720,7 @@ servidor.get("/user/history", logging, function (req, res) {
     html += '<link rel="stylesheet" href="/../css/footer_css.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="/../css/general_styles.css" type="text/css">\n';
     html += '<link rel="stylesheet" href="/../css/carrinho_css.css" type="text/css">\n';
+    html += '<link rel="icon" type="images/x-icon" href="/../images/logos/main.png">';
     html += '<script src="/../js/login.js"></script>\n';
     //HTML close head
     html += '</head>\n<body>';
