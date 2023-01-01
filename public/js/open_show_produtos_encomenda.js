d7e48e80741970a9e77806dd76ffdeaa63a1756a
auzunder <edgarnogueiratavares@gmail.com>
2023-01-01 18:46:21 +0000

    eventos

--- public/js/open_show_produtos_encomenda.js
+++ public/js/open_show_produtos_encomenda.js
@@ -1,12 +1,12 @@
 var aberto = false;
-document.getElementById('hide_show_produtos').onclick = function () {
+function hide_show_produtos(encomenda) {
     if (aberto) {
-        document.getElementById('encomenda_produtos').classList.add('hidden');
-        document.getElementById('hide_show_produtos_seta').classList.remove('rotate90');
+        document.getElementById('encomenda_produtos_' + encomenda).classList.add('hidden');
+        document.getElementById('hide_show_produtos_seta_' + encomenda).classList.remove('rotate90');
         aberto = false;
     } else {
-        document.getElementById('encomenda_produtos').classList.remove('hidden');
-        document.getElementById('hide_show_produtos_seta').classList.add('rotate90');
+        document.getElementById('encomenda_produtos_' + encomenda).classList.remove('hidden');
+        document.getElementById('hide_show_produtos_seta_' + encomenda).classList.add('rotate90');
         aberto = true;
     }
 }
